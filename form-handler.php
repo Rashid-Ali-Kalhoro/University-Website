e010e51916d849274f393c958432f53a0045efb5
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-06-25 19:44:18 +0500

    added social links

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'rkkalhoro09@gmail.com';
+$email_from = 'info@yourwebsite.com';
 
 $email_subject = 'New Form Submission';
 
@@ -21,4 +21,5 @@ $headers .= "Reply-To: $visitor_email\r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
+
 ?>
